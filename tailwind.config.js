7d77cab75456d3217b0d115021a9aa8497fc8225
ProAlexis <99012554+ProAlexis@users.noreply.github.com>
2026-01-05 23:41:50 +0100

    maj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', // <--- ON AJOUTE CETTE LIGNE ICI
   theme: {
-    extend: {},
+    extend: {
+      // Tu peux laisser vide ou avec tes extensions actuelles
+    },
   },
   plugins: [],
 }
